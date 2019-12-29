62708303b7c56e071e1e078c43df70f0749f2213
dyazincahya <cahyadyazin@yahoo.com>
2019-12-30 00:09:33 +0700

    big push

--- application/views/willkommen_seite.php
+++ application/views/willkommen_seite.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>HOLY QURAN - API V1.0 - BY KANG-CAHYA.COM</title>
+	<title>HOLY QURAN API - BY KANG-CAHYA.COM</title>
 	<link rel="shortcut icon" href="<?=base_url(IMAGES."icon/default.png");?>">
 	<link rel="stylesheet" href="<?=base_url(THEME."css/bootstrap.min.css");?>">
 	<link rel="stylesheet" href="<?=base_url(THEME."css/sticky-footer-navbar.css");?>">
@@ -24,7 +24,10 @@
 <body>
 	<div class="container">
 		<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-			<h1 class="display-4"><img src="<?=base_url(IMAGES."icon/default.png");?>" height="75" alt="Logo Icon"/> HOLY QURAN - API V1.0</h1>
+			<h1 class="display-4">
+				<img src="<?=base_url(IMAGES."icon/default.png");?>" height="75" alt="Logo Icon"/> 
+				HOLY QURAN API
+			</h1>
 			<div class="btn-toolbar mb-2 mb-md-0">
 				<div class="row">
 					<div class="col-3">
--- assets/quran/other/audio/index.php
+++
@@ -1,1 +0,0 @@
-<?php echo "Error 403: Forbidden Access!"; ?>
