61f0b68f867bd11cd35fa5e9614ddde05ab6967f
Kang Cahya <cahya@kct.co.id>
2021-06-18 09:36:20 +0700

    ;

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = "https://api-quran-id.herokuapp.com/"
+$config['base_url'] = "https://api-quran-id.herokuapp.com/";
 
 /*
 |--------------------------------------------------------------------------
